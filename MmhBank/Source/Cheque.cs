df3fb5ca4ba22e6b9b8e86f02491dc2043e45ebc
MMHaba <moussamoisejk@gmail.com>
2018-03-24 05:04:55 -0600

    POO clase creation

---
+++ MmhBank/Source/Cheque.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MmhBank.Source
+{
+   
+    
+    public class Cheque
+    {
+        public Cheque(int compteNumber,
+                        double interestRate, 
+                        int numberOfTransactions, 
+                        double interestRateMinimum,
+                        string description)
+        {
+            this.CompteNumber = compteNumber;
+            this.InterestRate = interestRate;
+            this.NumberOfTransactions = numberOfTransactions;
+            this.InterestRateMinimum = interestRateMinimum = 0.01;
+            this.Description = description;
+        }
+
+        public int CompteNumber { get; set; }
+        public double InterestRate { get; set; }
+
+        public int NumberOfTransactions { get; set; }
+        public double InterestRateMinimum { get; set; }
+
+        public string Description { get; set; }
+
+        public double CalculateInteret(double interestRate,double solde)
+        {
+            return 0;
+        }
+   
+
+
+    }
+}
---
+++ MmhBank/Source/Compte.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MmhBank.Source
+{
+    public class Compte
+    {
+        public Compte(int accountNumber,double interestRate,double balance,string description)
+        {
+            AccountNumber = accountNumber;
+            InterestRate = interestRate;
+            Balance = balance;
+            Description = description;
+
+        }
+        public int AccountNumber { get; set; }
+        public double InterestRate { get; set; }
+        public double Balance { get; set; }
+        public string Description { get; set; }
+    }
+}
